87470e09f203e1e5798aa93bbda5377c937c9be0
gbarbon <gianluca.barbon@gmail.com>
2014-03-10 15:49:20 +0100

    asfadfa

---
+++ hpc/hpc.1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: jian
+ *
+ * Created on 10 marzo 2014, 15.46
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
