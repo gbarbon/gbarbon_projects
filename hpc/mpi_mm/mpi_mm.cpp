98cb04332778d4c88132aca4e9eda12ef44004a0
dztracing <dztracing@gmail.com>
2014-03-12 13:50:02 +0100

    Aggiunte le free!

--- hpc/mpi_mm/mpi_mm.cpp
+++ hpc/mpi_mm/mpi_mm.cpp
@@ -218,6 +218,18 @@ int main(int argc, char *argv[]) {
         }
     }
 
+	free(A);
+	free(B);
+	free(C);
+	free(tmp);
+	free(tmpA);
+	free(tmpB);
+	free(Avett);
+	free(Bvett);
+	/*free(blocchiA);
+	free(blocchiB);
+	free(coord);*/
+	
     MPI_Finalize();
     return 0;
 }
