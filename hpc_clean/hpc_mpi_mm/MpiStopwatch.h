ce1bc874ba1ca52cbca808d78dd6758590fd1ff3
gbarbon <gianluca.barbon@gmail.com>
2014-03-26 10:22:34 +0100

    aggiunti stopwatch

---
+++ hpc_clean/hpc_mpi_mm/MpiStopwatch.h
@@ -0,0 +1,63 @@
+/*
+ * Stopwatch.h
+ *
+ *  Created on: 08/mag/2013
+ *      Author: henry
+ */
+
+#ifndef STOPWATCH_H_
+#define STOPWATCH_H_
+
+//#include <time.h>
+#include <mpi/mpi.h>
+
+typedef struct
+{
+	double _start;
+	double _end;
+} _stopwatch;
+
+typedef _stopwatch* Stopwatch;
+
+Stopwatch StopwatchCreate()
+{
+	Stopwatch res = (Stopwatch) malloc(sizeof(_stopwatch));
+	res->_start = 0;
+	res->_end = 0;
+	return res;
+}
+void StopwatchStart(Stopwatch chrono)
+{
+	chrono->_start = MPI_Wtime(); //clock(); //time(0);
+}
+void StopwatchStop(Stopwatch chrono)
+{
+	chrono->_end = MPI_Wtime();
+}
+float StopwatchGetElapsed(Stopwatch chrono)
+{
+	return ((float)(chrono->_end - chrono->_start));
+}
+int StopwatchIsValid(Stopwatch chrono)
+{
+	return chrono->_end >= chrono->_start;
+}
+void StopwatchPrintResolution()
+{
+	/*clock_t t1, t2;
+	t1 = t2 = clock();
+
+	// loop until t2 gets a different value
+	while(t1 == t2)
+		t2 = clock();*/
+    
+    double tick = MPI_Wtick() * 1000;
+	printf("%f ms.\n", tick);
+}
+void StopwatchPrintWithComment(const char* message, Stopwatch chrono)
+{
+	printf(message, StopwatchGetElapsed(chrono));
+}
+
+
+#endif /* STOPWATCH_H_ */
