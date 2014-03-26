b2f55f81177ebfcbbea260639a9b72649c7e8601
gbarbon <gianluca.barbon@gmail.com>
2014-03-26 12:25:16 +0100

    integrazione stopwatch

--- hpc_clean/hpc_mpi_mm/mm.c
+++ hpc_clean/hpc_mpi_mm/mm.c
@@ -1,7 +1,7 @@
 
-/* 
+/*
  * File:   mm.c
- * Author: 
+ * Author:
  *
  * Created on 12 marzo 2014, 15.20
  */
@@ -12,7 +12,7 @@
 
 /**
  * Computes multiplication
- * 
+ *
  * @param rows  Pointer to the rows offsetxn matrix
  * @param cols  Pointer to the cols offsetxn matrix (it's transposed)
  * @param n     Dimension of the original matrices
@@ -22,6 +22,10 @@
 double** mult(double** rows, double** cols, int n, int offset) {
     int i, j, k;
     double** res; /*data structure for resulting matrix*/
+
+    /*stopwatch start*/
+    Stopwatch watch = StopwatchCreate();
+    StopwatchStart(watch);
 
     res = matrix_creator(offset, offset);
     for (i = 0; i < offset; i++) {
@@ -33,12 +37,18 @@ double** mult(double** rows, double** cols, int n, int offset) {
             }
         }
     }
+
+    /*stopwatch stop*/
+    StopwatchStop(watch);
+    StopwatchPrintWithComment("Time in mult function is: %f\n", watch);
+    free(watch);
+
     return res;
 }
 
 /**
  * Send rows and cols of A and B to the interested worker
- * 
+ *
  * @param A     Pointer to the first nxn matrix
  * @param B     Pointer to the second nxn matrix
  * @param offset        Dimension of the "local" matrix of the worker
@@ -47,6 +57,11 @@ double** mult(double** rows, double** cols, int n, int offset) {
 void master_sender(double** A, double** B, int offset, int n) {
     int i, j, worker = 0;
     double * tempA, * tempB;
+
+    /*stopwatch start*/
+    Stopwatch watch = StopwatchCreate();
+    StopwatchStart(watch);
+
     for (j = 0; j < n; j += offset)
         for (i = 0; i < n; i += offset) {
             worker++; /*increment worker number*/
@@ -54,7 +69,7 @@ void master_sender(double** A, double** B, int offset, int n) {
             /*vectorize the two pieces of matrices in order to send them*/
             tempA = matrix_vectorizer(offset, n, &A[j]);
             tempB = matrix_vectorizer(offset, n, &B[i]);
-            
+
             /*MPI send of rows and cols. Notice tag 0 for rows, tag 1 for cols*/
             MPI_Send(tempA, offset * n, MPI_DOUBLE, worker, 0, MPI_COMM_WORLD);
             MPI_Send(tempB, offset * n, MPI_DOUBLE, worker, 1, MPI_COMM_WORLD);
@@ -62,28 +77,37 @@ void master_sender(double** A, double** B, int offset, int n) {
             free(tempA);
             free(tempB);
         }
+
+    /*stopwatch stop*/
+    StopwatchStop(watch);
+    StopwatchPrintWithComment("Time in master_sender function is: %f\n", watch);
+    free(watch);
 }
 
 /**
  * Receives results from workers and create the final matrix
- * 
- * @param n     Dimension of the final nxn matrix   
+ *
+ * @param n     Dimension of the final nxn matrix
  * @param offset        Dimension of one of the incoming matrices
- * @return res  Pointer to the final result nxn matrix       
+ * @return res  Pointer to the final result nxn matrix
  */
 double** master_receiver(int n, int offset) {
     int i, j, x, y, worker = 0;
     double** res, ** res_temp;
+
+    /*stopwatch start*/
+    Stopwatch watch = StopwatchCreate();
+    StopwatchStart(watch);
 
     res = matrix_creator(n, n);
     for (j = 0; j < n; j += offset)
         for (i = 0; i < n; i += offset) {
             worker++;
             double * res_vect = (double *) malloc(offset * offset * sizeof (double));
-            
+
             /*receives a piece of the final matrix in vector form, tag 2 used*/
             MPI_Recv(res_vect, offset * offset, MPI_DOUBLE, worker, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
+
             /*transform the received vector in a matrix*/
             res_temp = devectorizer(offset, offset, res_vect);
             free(res_vect);
@@ -95,11 +119,16 @@ double** master_receiver(int n, int offset) {
             freematrix(offset, res_temp);
         }
 
+    /*stopwatch stop*/
+    StopwatchStop(watch);
+    StopwatchPrintWithComment("Time in master_receiver function is: %f\n", watch);
+    free(watch);
+
     return res;
 }
 
 /**
- * 
+ *
  * @param argc
  * @param argv Used in order to give the dimension of the matrices
  * @return 0
@@ -126,7 +155,14 @@ int main(int argc, char *argv[]) {
     /*show who I am*/
     /*printf("I'm process %d\n", myrank);*/
 
+    /*stopwatch start*/
+    Stopwatch watch = StopwatchCreate();
+
     if (myrank == 0) {
+
+        /*stopwatch start*/
+        StopwatchStart(watch);
+
         /* matrix creation */
         A = matrix_creator(n, n);
         B = matrix_creator(n, n);
@@ -144,6 +180,9 @@ int main(int argc, char *argv[]) {
         /*printf("Master has just passed the master_sender funct\n\n");*/
 
     } else {
+        /*stopwatch start*/
+        StopwatchStart(watch);
+        
         /*data structure for incoming rows & cols*/
         double** rows;
         double** cols;
@@ -176,9 +215,13 @@ int main(int argc, char *argv[]) {
         MPI_Send(res_vect, offset * offset, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
         freematrix(offset, res);
         free(res_vect);
+        
+        /*stopwatch stop*/
+        StopwatchStop(watch);
+        StopwatchPrintWithComment("Worker total time: %f\n", watch);
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
 
     /*collect all the stuff*/
     if (myrank == 0) {
@@ -191,8 +234,14 @@ int main(int argc, char *argv[]) {
         freematrix(n, A);
         freematrix(n, B);
         freematrix(n, res);
+
+        /*stopwatch stop*/
+        StopwatchStop(watch);
+        StopwatchPrintWithComment("Master total time: %f\n", watch);
+
     }
 
+    free(watch);
     MPI_Finalize();
     return 0;
 }
