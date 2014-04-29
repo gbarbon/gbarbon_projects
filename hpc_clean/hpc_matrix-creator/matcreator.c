0e5dd622afbb3e0f08e7950dfd3ce9e9580f838d
gbarbon <gianluca.barbon@gmail.com>
2014-04-29 18:16:26 +0200

    Versione 0.1 matcreator e modifiche a to_do

--- hpc_clean/hpc_matrix-creator/matcreator.c
+++ hpc_clean/hpc_matrix-creator/matcreator.c
@@ -1,6 +1,6 @@
 /* 
  * File:   matcreator.c
- * Author: maomao
+ * Author: 
  *
  * Created on 29 aprile 2014, 17.35
  */
@@ -13,7 +13,30 @@
  * 
  */
 int main(int argc, char** argv) {
+    double ** mat;
+    int dim;
+    char filename[256];
 
+    /*getting dimension from argv[1]*/
+    dim = atoi(argv[1]);
+
+    /*matrix creation*/
+    mat = matrix_creator(dim, dim);
+    if (mat == NULL) {
+        printf("Error in matrix creation!!\n");
+        return 0;
+    }
+    
+    /*filename creation*/
+    snprintf(filename, sizeof filename, "mat%d.csv", dim);
+
+    /*matrix saving*/
+    if (matrix_writer(dim, mat, filename) == 0) {
+        printf("Error in matrix writing!!\n");
+        return 0;
+    }
+    
+    printf("A file .csv with dimension %dx%d with name %s has successfully been created!!\n", dim, dim, filename);
     return (EXIT_SUCCESS);
 }
 
