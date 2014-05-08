8e5231c073bf01e892b669a8a885973f0d9f0347
gbarbon <gianluca.barbon@gmail.com>
2014-05-08 15:25:54 +0200

    Upload ready per test

--- hpc_clean/launcher.py
+++ hpc_clean/launcher.py
@@ -37,19 +37,22 @@ def common(args):
 	
 def loops():
 	print '*** Starting loops ***'
-	nprocs = ['5','17']
+	#nprocs = ['5','17']
+	nprocs = ['5','10','17']
 	execs = ['hpc_mpi_mm/bin/hpc_mpi_mm', 'hpc_mpi_mm-fast/bin/hpc_mpi_mm-fast', 'hpc_mpi_cannon/bin/hpc_mpi_cannon', 'hpc_mpi_farm/bin/hpc_mpi_farm']
 	optimization = [' ','-op ']
-	dims = ['16','64','256']
+	#dims = ['16','64','256']
+	dims = ['192','768','1920']
 	configs = [' 0 0', ' 0 1', ' 0 2', ' 1 0', ' 1 1', ' 1 2',] #first digist is for i/o on/off, second digit is for load_func low/medium/high
 	for nproc in nprocs:
 		for exe in execs:
 			for op in optimization:
 				for dim in dims:
 					for config in configs:
-						temp_args = "mpiexec -n " + nproc + " ./" + exe + op + dim + config
-						args = temp_args.split()
-						common(args)
+						for n in range(1, 6):
+							temp_args = "mpiexec -n " + nproc + " ./" + exe + op + dim + config
+							args = temp_args.split()
+							common(args)
 	print '*** Ending loops ***'
 	
 if __name__ == '__main__':
