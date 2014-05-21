9689c63d9a69d1e53beb2180bcaf7d6266262f0d
gbarbon <gianluca.barbon@gmail.com>
2014-05-21 20:11:43 +0200

    Aggiunto launcher per test mac anche con nuova versione linear

---
+++ hpc_clean/launcher_mac.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+launcher.py
+
+Created by Jian Banzai on 2014-05-06.
+Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+import subprocess
+
+def main():
+	makeall()	
+	loops()
+	
+#make all versions
+def makeall(): 
+	print '*** Make process starting for all versions ***'
+	dirs = ['hpc_mpi_mm','hpc_mpi_mm-fast','hpc_mpi_cannon','hpc_mpi_farm','hpc_linear_mm']
+	options = ['','opti']
+	for path in dirs:
+		os.chdir(path) #move into the desired path
+		for opt in options:
+			shellcom = "make " + opt
+			shellcom = shellcom.split()
+			common(shellcom)
+		os.chdir("..") #move back to the previous path
+	print '*** Make process finished ***'
+	
+def common(args):
+	popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+	popen.wait()
+	output = popen.stdout.read()
+	print output
+	
+def loops():
+	print '*** Starting loops ***'
+	#nprocs = ['5','17']
+	nprocs = ['5','10','17']
+	execs = ['hpc_mpi_mm/bin/hpc_mpi_mm', 'hpc_mpi_mm-fast/bin/hpc_mpi_mm-fast', 'hpc_mpi_cannon/bin/hpc_mpi_cannon', 'hpc_mpi_farm/bin/hpc_mpi_farm']
+	optimization = [' ','-op ']
+	#dims = ['16','64','256']
+	#dims = ['192','768','960']
+	#configs = [' 0 0', ' 0 1', ' 0 2', ' 1 0', ' 1 1', ' 1 2',] #first digist is for i/o on/off, second digit is for load_func low/medium/high
+	dimconfigs = ['192 0 0','1920 1 2']
+	
+	for nproc in nprocs:
+		for exe in execs:
+			for op in optimization:
+				for dimconfig in dimsconfigs:
+					temp_args = "mpiexec -n " + nproc + " ./" + exe + op + dimconfig
+					args = temp_args.split()
+					common(args)
+	
+	#loop for 'hpc_linear_mm/bin/hpc_linear_mm'
+	for op in optimization:
+		for dimconfig in dimsconfigs:
+			temp_args = "mpiexec -n 1 ./hpc_linear_mm/bin/hpc_linear_mm" + op + dimconfig
+			args = temp_args.split()
+			common(args)
+			
+	print '*** Ending loops ***'
+	
+if __name__ == '__main__':
+	main()
+
